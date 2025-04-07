e9197a60f858046b95345120ebcfa47e0be0cddc
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-04-07 11:46:32 -0300

    refatorando classe teste

--- src/main/java/challenge/fiap/Test.java
+++ src/main/java/challenge/fiap/Test.java
@@ -3,7 +3,7 @@ package challenge.fiap;
 import challenge.fiap.models.Failure;
 import challenge.fiap.models.Report;
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
         var falha = new Failure();
         var relatorio = new Report();
